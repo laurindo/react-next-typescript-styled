da5d2e09323819686a5f0425c1048c87dd34f5ee
Daniel Laurindo <dslaurindo@gmail.com>
2021-04-21 16:58:40 -0300

    Updating folder __mocks__

--- src/tests/__mocks__/axios.js
+++ src/tests/__mocks__/axios.js
@@ -4,15 +4,51 @@ export default {
       data: [
         {
           id: 1,
-          title: 'Post 1'
+          title: 'Harry Potter - Prisioner of Azkaban'
         },
         {
           id: 2,
-          title: 'Post 2'
+          title: 'React + Redux - Book'
         },
         {
           id: 3,
-          title: 'Post 3'
+          title: 'Microservice in action'
+        },
+        {
+          id: 4,
+          title: 'Microfrontend in Action'
+        },
+        {
+          id: 5,
+          title: 'Guide Bitcoin'
+        },
+        {
+          id: 6,
+          title: 'Blockchain from zero'
+        },
+        {
+          id: 7,
+          title: 'GraphQL - Deep understading'
+        },
+        {
+          id: 8,
+          title: 'Star Wars - The beggining'
+        },
+        {
+          id: 9,
+          title: 'Heavy Metal - The history of everything'
+        },
+        {
+          id: 10,
+          title: 'Rhapsody of Fire'
+        },
+        {
+          id: 11,
+          title: 'Game of Thrones - pt.01'
+        },
+        {
+          id: 12,
+          title: 'Cuisine - The best foods'
         }
       ]
     })
